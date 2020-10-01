ad30edb3885b8eeae0b2fe2014ee741e6fa86d0d
Ryan Cahela <ryancahela@gmail.com>
2020-09-30 17:05:07 -0700

    set up header and footer using react-bootstrap

--- Course_Project/frontend/src/App.js
+++ Course_Project/frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Proshop!</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome to Proshop!</h1>
+        </main>
+      </Container>
+      <Footer />
     </div>
   );
 }
